ee1aea427be63e1932a6f544977b3ccae2de6caa
iVan Vitali <webcoder.ivan@gmail.com>
2017-04-05 11:21:15 -0300

    angular1#1 Se creo la carpeta app, se creo el modulo IvanApp, se incluyo la libreria de angular dentro del archivo index.html y tambien se incluyo el archivo app.js dentro del archivo index.html

---
+++ app/app.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+    
+    angular.module('IvanApp', [
+        // Angular modules
+
+        // Custom modules
+        
+        // 3rd Party Modules
+        
+    ]);
+    
+ }());
